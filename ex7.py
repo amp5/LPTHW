f55588527ed08d28957e283bbd6474cce4dceb34
amp5 <aplassaras@gmail.com>
2013-09-03 18:00:53 -0700

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,26 @@
+#prints what is in the quotes
+print "Mary had a little lamb."
+# prints what is in the quoted as well as includes a string within a string. adds in 'snow'
+print "Its fleece as white as %s." % 'snow'
+#prints what is in the quotes
+print "And everywhere that Mary went."
+#prints "." ten times
+print "." * 10 # what'd that do?
+#defines a variable and assigns a letter to each variable (do this 12 times)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try removing it and see what happens
+#prints variables 1 - 6 and spells 'Cheese'. with the ',' after end6 variable lines 25 ans 26 are printed on same line.Without ',' printed on different lines.
+print end1 + end2 + end3 + end4 + end5 + end6 
+print end7 + end8 + end9 + end10 + end11 + end12
