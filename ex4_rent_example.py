3a7f8d07529a19345d54467d866288c4748af75a
amp5 <aplassaras@gmail.com>
2013-09-03 17:59:23 -0700

    Create ex4_rent_example.py
    Extra credit working on exercise 4

---
+++ ex4_rent_example.py
@@ -0,0 +1,10 @@
+rent = 945.0
+utilities = 10.0
+medical_bills = 60.0
+total_expenses = rent + utilities + medical_bills
+wage = 14.0 * 20 * 4
+end_of_month_total = wage - total_expenses
+
+print "If I work the minimum at this part-time what will I earn? (30 hours)", wage
+
+print "Will my current expenses will I be able to live off of this for at least a month?", end_of_month_total
