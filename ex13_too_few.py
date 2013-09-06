a55a65e1a569b3232cb2baa94f60b3f3db5584a1
amp5 <aplassaras@gmail.com>
2013-09-05 20:05:31 -0700

    Create ex13_too_few.py

---
+++ ex13_too_few.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, brunch, dinner = argv
+
+print "This is the name of your script:", script
+print "You will have this for brunch:", brunch
+print "You will have this for dinner:", dinner
