5b39af83cce03538b4149047fa8739611db25180
amp5 <aplassaras@gmail.com>
2013-09-05 20:48:48 -0700

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -4,9 +4,9 @@ from sys import argv
 #defining two arguments, script and user_name
 #which you will have to set these parameters as you call out the script
 #in the command line
-script, user_name = argv
+script, user_name, level_experience = argv
 #defining the variable 'prompt' to use later
-prompt = '>'
+prompt = 'enter information here'
 
 #printing first string, and calling in two arguments defined earlier
 print "Hi %s, I'm the %s script." % (user_name, script)
@@ -27,9 +27,14 @@ print "What kind of computer do you have?"
 #asks for raw input, using variable 'prompt'
 computer = raw_input(prompt)
 
+print "So you are at %s level of experience, right?" % level_experience
+experience = raw_input(prompt)
+
+
 #prints multi-line string, aggregating above-entered raw inputs
 print """
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
 And you have a %r computer. Nice.
-""" % (likes, lives, computer)
+Lastly, your level of experience is %r. Not bad.
+""" % (likes, lives, computer, experience)
