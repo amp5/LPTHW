731ca4eb196f799450b0e01a0740636be655fa8d
amp5 <aplassaras@gmail.com>
2013-09-05 20:05:05 -0700

    Create ex13_too_many.py

---
+++ ex13_too_many.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, kitty_1, kitty_2, kitty_3, kitty_4 = argv
+
+print "The script is called:", script
+print "Your first kitty's name is:", kitty_1
+print "Your second kitty's name is:", kitty_2
+print "Your third kitty's name is:", kitty_3
+print "your final kitty's name is:", kitty_4
