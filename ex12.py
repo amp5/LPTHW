434849b1145e1309e9e679f48f218cf354af03ad
amp5 <aplassaras@gmail.com>
2013-09-05 19:22:51 -0700

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,6 +1,10 @@
+#defines variables below and will display the script in the command line.
+#also allows for user to input raw data
 age = raw_input ("How old are you? ")
 height = raw_input ("How tall are you? ")
 weight = raw_input ("How much do you weigh? ")
 
+#prints the aggregation of all three variables and the inputs
+#entered by users
 print "So, you're %r old, %r tall, %r heavy." % (
-    age, height, weight)  
+    age, height, weight)
