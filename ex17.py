fe75ee9a57a5a575fd15c05c32562b9dcd1a9de0
amp5 <aplassaras@gmail.com>
2013-10-22 14:48:13 -0700

    Update ex17.py

--- ex17.py
+++ ex17.py
@@ -1,75 +1,38 @@
 from sys import argv
-
 from os.path import exists
-
-
 
 script, from_file, to_file = argv
 
-
-
 print "Copying from %s to %s" % (from_file, to_file)
 
-
-
 #we could do these two on one line too, now?
-
 input = open(from_file)
-
 indata = input.read()
-
-
 
 print "The input file is %d bytes long" % len(indata)
 
-
-
 print "Does the output file exist? %r" % exists(to_file)
-
 print "Ready, hit RETIRN to continue, CTRL-C to abort."
-
 raw_input()
 
-
-
 output = open(to_file, 'w')
-
 output.write(indata)
-
-
 
 print "Alright, all done."
 
-
-
 output.close()
-
 input.close()
 
 
-
 #From here on out I will be putting my notes below
-
 #Instead of going line by line...
 
-
-
 #Things I need to note: In command line
-
 #I will type in the name of this file (ex17.py)
-
 #test.txt as the from_file
-
 #and copied.txt as the to_file
-
-
 
 #when we import the 'exists' command it returns a True if a file exists, based on it's name in a string as an argument. False, if not. 
 
-
-
-
-
-#Questions to answer late:
-
+#Questions to answer later:
 #what does len(indata) mean?
